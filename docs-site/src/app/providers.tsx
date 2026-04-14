96260fb23aed8bed0619621a1b2a9bd60365dd7e
Arun Saragadam <iarunsaragadam@gmail.com>
2026-04-14 17:57:29 +0100

    feat(keyboard-shortcut): introduce global Alt-to-reveal shortcut system (#124)

--- docs-site/src/app/providers.tsx
+++ docs-site/src/app/providers.tsx
@@ -2,19 +2,22 @@
 
 import { ThemeProvider } from '@refraction-ui/react-theme'
 import { ToastProvider } from '@refraction-ui/react-toast'
+import { ShortcutProvider } from '@refraction-ui/react'
 import { MobileNavProvider } from '@/components/mobile-nav-context'
 import { FrameworkProvider } from '@/components/framework-context'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider defaultMode="light" attribute="class">
-      <ToastProvider>
-        <FrameworkProvider>
-          <MobileNavProvider>
-            {children}
-          </MobileNavProvider>
-        </FrameworkProvider>
-      </ToastProvider>
+      <ShortcutProvider>
+        <ToastProvider>
+          <FrameworkProvider>
+            <MobileNavProvider>
+              {children}
+            </MobileNavProvider>
+          </FrameworkProvider>
+        </ToastProvider>
+      </ShortcutProvider>
     </ThemeProvider>
   )
 }
