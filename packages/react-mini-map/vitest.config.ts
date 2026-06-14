ec9539713401d3e1d190a4b0c5f980ef5a62f133
Arun Saragadam <arun88m@gmail.com>
2026-06-14 23:36:01 +0100

    test: interaction tests for canvas components

--- packages/react-mini-map/vitest.config.ts
+++ packages/react-mini-map/vitest.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import path from 'node:path'
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: /^@refraction-ui\/([^/]+)(.*)$/,
+        replacement: path.resolve(__dirname, "../../packages/$1/src$2")
+      }
+    ]
+  },
   test: {
     environment: 'node',
     passWithNoTests: true,
